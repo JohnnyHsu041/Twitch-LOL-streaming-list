3bd4c895b44c4331fec698826ae4fbd939d8a23a
Johnny Hsu <a209328641@gmail.com>
2022-06-04 16:31:06 +0800

    file path revise

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/js/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
